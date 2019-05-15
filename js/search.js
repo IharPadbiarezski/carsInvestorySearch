884f877ecbe3a516a842f4fce45c1f4a6987468f
IharPadbiarezski <yb.igrok.by@gmail.com>
2019-05-15 16:28:07 +0300

    feat: displaying another filters

--- js/search.js
+++ js/search.js
@@ -218,6 +218,41 @@ year.addEventListener("input", e => {
   filterCars();
 });
 
+const min = document.querySelector("#min");
+min.addEventListener("input", e => {
+  searchFields.min = Number(e.target.value);
+  // execute the filter cars based on the items that we have in the search fields object
+  filterCars();
+});
+
+const max = document.querySelector("#max");
+max.addEventListener("input", e => {
+  searchFields.max = Number(e.target.value);
+  // execute the filter cars based on the items that we have in the search fields object
+  filterCars();
+});
+
+const doors = document.querySelector("#doors");
+doors.addEventListener("input", e => {
+  searchFields.doors = Number(e.target.value);
+  // execute the filter cars based on the items that we have in the search fields object
+  filterCars();
+});
+
+const transmission = document.querySelector("#transmission");
+transmission.addEventListener("input", e => {
+  searchFields.transmission = e.target.value;
+  // execute the filter cars based on the items that we have in the search fields object
+  filterCars();
+});
+
+const color = document.querySelector("#color");
+color.addEventListener("input", e => {
+  searchFields.color = e.target.value;
+  // execute the filter cars based on the items that we have in the search fields object
+  filterCars();
+});
+
 function clearHTML() {
   // select the container
   const container = document.querySelector("#result");
@@ -251,12 +286,17 @@ function showCars(cars) {
 function filterCars() {
   const result = getCars()
     .filter(filterMake)
-    .filter(filterYear);
+    .filter(filterYear)
+    .filter(filterMinPrice)
+    .filter(filterMaxPrice)
+    .filter(filterDoors)
+    .filter(filterTransmission)
+    .filter(filterColor);
 
   if (result.length) {
     showCars(result);
   } else {
-    alert("No results found");
+    // alert("No results found");
   }
 }
 
@@ -271,6 +311,46 @@ function filterMake(car) {
 function filterYear(car) {
   if (searchFields.year) {
     return car.year === searchFields.year;
+  } else {
+    return car;
+  }
+}
+
+function filterMinPrice(car) {
+  if (searchFields.min) {
+    return car.price >= searchFields.min;
+  } else {
+    return car;
+  }
+}
+
+function filterMaxPrice(car) {
+  if (searchFields.max) {
+    return car.price <= searchFields.max;
+  } else {
+    return car;
+  }
+}
+
+function filterDoors(car) {
+  if (searchFields.doors) {
+    return car.doors === searchFields.doors;
+  } else {
+    return car;
+  }
+}
+
+function filterTransmission(car) {
+  if (searchFields.transmission) {
+    return car.transmission === searchFields.transmission;
+  } else {
+    return car;
+  }
+}
+
+function filterColor(car) {
+  if (searchFields.color) {
+    return car.color === searchFields.color;
   } else {
     return car;
   }
